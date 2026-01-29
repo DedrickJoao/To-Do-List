f2f99b2cd71c63bb90dc75005e685d67c2a5ad33
DedrickJoao <dedrickdomingos.domingos@gmail.com>
2026-01-29 15:38:43 +0200

    Fisrt-comit

---
+++ script.js
@@ -0,0 +1,71 @@
+const userName = "Dédrick";
+
+document.getElementById("welcomeMessage").innerText = `Hey, ${userName}!`;
+
+alert("Bien Venido Dédick!!! Small wins add up. One Task At A Time.");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Renderizar tarefas ao abrir a app
+renderTasks();
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    if (!input.value) return;
+
+    tasks.push({
+        text: input.value,
+        status: "Pending"
+    });
+
+    input.value ="";
+    saveAndRender();
+
+}
+
+function changeStatus(index, newStatus) {
+    tasks[index].status = newStatus;
+    saveAndRender();
+}
+
+function saveAndRender() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+        renderTasks();
+}
+
+function renderTasks() {
+    document.getElementById("pending").innerHTML = "";
+    document.getElementById("progress").innerHTML = "";
+    document.getElementById("completed").innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.className = getClass(task.status);
+
+        li.innerHTML = `
+            ${task.text}
+            <select onchange="changeStatus(${index}, this.value)">
+                <option ${task.status === "Pending" ? "selected" : ""}>Pending</option>
+                <option ${task.status === "In Progress" ? "selected" : ""}>In Progress</option>
+                <option ${task.status === "Completed" ? "selected" : ""}>Completed</option>
+            </select>
+        `;
+
+        document.getElementById(getColumn(task.status)).appendChild(li);
+    });
+}
+
+
+function getColumn(status) {
+    if (status == "Pending") return "pending";
+    if (status == "In Progress") return "progress";
+    return "completed";
+}
+
+function getClass(status) {
+    if (status == "Pending") return "pending";
+    if (status == "In Progress") return "progress";
+    return "completed";
+}
+
+   
